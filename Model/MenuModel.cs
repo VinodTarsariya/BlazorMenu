fbf8acc1340091ad66821400d68195b6d4b75f8b
Vinod-Ghanshyam <dasgshyam@gmail.com>
2020-05-03 01:43:52 +0530

    Dynamic Menu created up to five level.

---
+++ Model/MenuModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorMenu.Model
+{
+    public class MenuModel
+    {
+        public string MenuId { get; set; }
+        public string MenuName { get; set; }
+        public string ParentId { get; set; }
+
+        public string IconClass { get; set; }
+               
+       
+    }
+}
